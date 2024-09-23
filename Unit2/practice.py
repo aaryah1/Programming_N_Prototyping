7a7af803f10b5bfa1073c1ff539a530bdcd96fe7
A.Haque <144722322+aaryah1@users.noreply.github.com>
2024-09-23 12:10:13 -0400

    Update practice.py

--- Unit2/practice.py
+++ Unit2/practice.py
@@ -3,3 +3,11 @@ r = int(input("What is the radius of your circle?"))
 c = 2*r*math.pi
 a = math.pi*r**2
 print("c=",c," a=" ,a)
+
+
+import math
+r = int(input("What is the radius of your circle?"))
+h = int(input("What is the height of your circle?"))
+v = math.pi*r**2*h
+print("v=" ,v)
+
