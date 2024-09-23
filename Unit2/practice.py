95be114e8cb13749ecb5ac8d32d338478cf61597
A.Haque <144722322+aaryah1@users.noreply.github.com>
2024-09-23 12:15:02 -0400

    Update practice.py

--- Unit2/practice.py
+++ Unit2/practice.py
@@ -11,3 +11,9 @@ h = int(input("What is the height of your circle?"))
 v = math.pi*r**2*h
 print("v=" ,v)
 
+#round to the hundereths
+import math
+r = int(input("What is the radius of your circle?"))
+h = int(input("What is the height of your circle?"))
+v = round(math.pi*r**2*h,2)
+print("v=" ,v)
