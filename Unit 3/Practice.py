3438a5e5bb6db99783c75974105fb302dc25ed5c
A.Haque <144722322+aaryah1@users.noreply.github.com>
2024-10-08 12:32:09 -0400

    Update Practice.py

--- Unit 3/Practice.py
+++ Unit 3/Practice.py
@@ -7,3 +7,12 @@ def mult_function(x,y):
 mult_function(100,750)
 mult_function(7,3)
 mult_function(5,4)
+
+
+   def find_rect_area(w,l):
+    area = w*l
+    (print("calculating area..."))
+    return area
+        
+answer = find_rect_area(4,7)
+print(answer)
